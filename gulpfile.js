d89456f99c5cb496db3d1599c4416a7767e1a4bd
Wesley Brown <wesleybrown@Wesleys-MacBook.local>
2017-04-18 20:55:07 +0100

    Gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-gulp.task('');
+var jsSources = ['components/scripts/*.js'];
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+     .pipe(concat('script.js'))
+     .pipe(gulp.dest('builds/development/js'))
+});
+
+
