f979920911bd1b22b4e08454e5ce5fcc904e2bb9
Wesley Brown <wesleybrown@Wesleys-MacBook.local>
2017-04-19 11:29:07 +0100

    Created Default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
+    compass = require('gulp-compass');
 
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function() {
     gulp.src(jsSources)
@@ -12,4 +14,15 @@ gulp.task('js', function() {
      .pipe(gulp.dest('builds/development/js'))
 });
 
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+     .pipe(compass({
+     	sass: 'components/sass',
+     	image:'builds/development/images',
+     	style: 'expanded'
+     }))
+     .on('error', gutil.log)
+     .pipe(gulp.dest('builds/development/css'))
+});
 
+gulp.task('default', ['js','compass']);
